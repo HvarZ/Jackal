a0df48aee420367f7d3acfc70ebb8828721109a9
HvarZ <hvarzahar@gmail.com>
2022-07-13 12:35:50 +0300

    add face detect test

---
+++ src/test/java/FaceDetectTest.java
@@ -0,0 +1,33 @@
+import eleron.jackal.DetectException;
+import eleron.jackal.Detector;
+import eleron.jackal.IDetectable;
+import eleron.jackal.JackalTypes;
+
+public class FaceDetectTest {
+    public static void main(String[] args) {
+        try {
+            IDetectable detector = new Detector(JackalTypes.FRONTAL_FACE);
+            detector.faceDetect(
+            "src/test/resources/TestFaceDetectImage.png",
+            "src/test/resources/TestFaceDetectResultImage.png");
+            detector.faceDetect(
+            "src/test/resources/TestFaceDetectImageOnePerson.png",
+            "src/test/resources/TestFaceDetectResultOnePerson.png"
+            );
+
+            IDetectable detector_2 = new Detector(JackalTypes.FRONTAL_FACE_2);
+            detector_2.faceDetect(
+            "src/test/resources/TestFaceDetectImage.png",
+            "src/test/resources/TestFaceDetectResultImage_2.png");
+
+            detector_2.faceDetect(
+            "src/test/resources/TestFaceDetectImageOnePerson.png",
+            "src/test/resources/TestFaceDetectResultOnePerson_2.png"
+            );
+
+
+        } catch (DetectException e) {
+            e.printStackTrace();
+        }
+    }
+}
