bf908fb5c73394b787cce7000e42b7c008b76c85
HvarZ <hvarzahar@gmail.com>
2022-07-14 10:57:03 +0300

    add default JackalType

--- src/main/java/eleron/jackal/JackalTypes.java
+++ src/main/java/eleron/jackal/JackalTypes.java
@@ -1,6 +1,7 @@
 package eleron.jackal;
 
 public final class JackalTypes {
+    public static final int DEFAULT = 0;
     public static final int FRONTAL_FACE = 1;
     public static final int FRONTAL_FACE_2 = 2;
 }
