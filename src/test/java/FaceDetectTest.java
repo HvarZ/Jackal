763e0ba4a9ac76ca20b8cbe1b87908c16af3dfd2
HvarZ <hvarzahar@gmail.com>
2022-07-14 10:57:41 +0300

    change resources structure and parameter change

--- src/test/java/FaceDetectTest.java
+++ src/test/java/FaceDetectTest.java
@@ -6,23 +6,29 @@ import eleron.jackal.JackalTypes;
 public class FaceDetectTest {
     public static void main(String[] args) {
         try {
-            IDetectable detector = new Detector(JackalTypes.FRONTAL_FACE);
+            IDetectable detector = new Detector();
             detector.faceDetect(
-            "src/test/resources/TestFaceDetectImage.png",
-            "src/test/resources/detectResults/TestFaceDetectResultImage.png");
+            "src/test/resources/main/TestFaceDetectImage.png",
+            "src/test/resources/detectResults/TestFaceDetectResultImage.png",
+                    JackalTypes.FRONTAL_FACE
+                    );
             detector.faceDetect(
-            "src/test/resources/TestFaceDetectImageOnePerson.png",
-            "src/test/resources/detectResults/TestFaceDetectResultOnePerson.png"
+            "src/test/resources/main/TestFaceDetectImageOnePerson.png",
+            "src/test/resources/detectResults/TestFaceDetectResultOnePerson.png",
+                    JackalTypes.FRONTAL_FACE
             );
 
-            IDetectable detector_2 = new Detector(JackalTypes.FRONTAL_FACE_2);
+            IDetectable detector_2 = new Detector();
             detector_2.faceDetect(
-            "src/test/resources/TestFaceDetectImage.png",
-            "src/test/resources/detectResults/TestFaceDetectResultImage_2.png");
+            "src/test/resources/main/TestFaceDetectImage.png",
+            "src/test/resources/detectResults/TestFaceDetectResultImage_2.png",
+                    JackalTypes.FRONTAL_FACE_2
+            );
 
             detector_2.faceDetect(
-            "src/test/resources/TestFaceDetectImageOnePerson.png",
-            "src/test/resources/detectResults/TestFaceDetectResultOnePerson_2.png"
+            "src/test/resources/main/TestFaceDetectImageOnePerson.png",
+            "src/test/resources/detectResults/TestFaceDetectResultOnePerson_2.png",
+                    JackalTypes.FRONTAL_FACE_2
             );
 
 
