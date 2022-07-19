38a161c6ae22fc10a217b166c7ba83b8c65dd6f7
HvarZ <hvarzahar@gmail.com>
2022-07-19 16:01:48 +0300

    add two new interface functions

--- src/main/java/eleron/jackal/IDetectable.java
+++ src/main/java/eleron/jackal/IDetectable.java
@@ -4,4 +4,8 @@ public interface IDetectable {
     void faceDetect(String pathRead, String pathWrite, int cascadeVariant) throws DetectException;
 
     void faceDetectAndCut(String pathRead, String pathWrite, int cascadeVariant) throws DetectException;
+
+    void magicWand(String pathRead, String pathWrite, int[] kernelSettings, int[] preparingSettings) throws DetectException;
+
+    void findAndBlur(String pathRead, String pathWrite) throws DetectException;
 }
