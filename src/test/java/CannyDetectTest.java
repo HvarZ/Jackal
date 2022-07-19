c66b48514b68fabb3da38a87f0296ad5988a01c2
HvarZ <hvarzahar@gmail.com>
2022-07-19 16:02:25 +0300

    rename function what use Canny Detect

--- src/test/java/CannyDetectTest.java
+++ src/test/java/CannyDetectTest.java
@@ -5,11 +5,11 @@ public class CannyDetectTest {
     public static void main(String[] args) {
         try {
             Detector detector = new Detector();
-            detector.edgeDetect(
+            detector.magicWand(
                     "src/test/resources/main/TestCannyDetectImage.png",
                     "src/test/resources/cannyDetectResults/TestCannyDetectResult.png",
-                    new int[]{30, 60, 3},
-                    new int[]{3, 3, 200}
+                    new int[]{50, 200, 3},
+                    new int[]{3, 3, 235}
             );
 
         } catch (DetectException e) {
