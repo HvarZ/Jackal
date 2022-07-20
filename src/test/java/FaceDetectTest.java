b87c77a4585ddb863cdedc7c0ee7aade44d1a946
HvarZ <hvarzahar@gmail.com>
2022-07-20 14:34:53 +0300

    clean tests

--- src/test/java/FaceDetectTest.java
+++ src/test/java/FaceDetectTest.java
@@ -14,17 +14,6 @@ public class FaceDetectTest {
             "src/test/resources/detectResults/TestFaceDetectResultOnePerson.png"
             );
 
-            Detector detector_2 = new Detector();
-            detector_2.backgroundBlur(
-            "src/test/resources/main/TestFaceDetectImage.png",
-            "src/test/resources/detectResults/TestFaceDetectResultImage_2.png"
-            );
-
-            detector_2.backgroundBlur(
-            "src/test/resources/main/TestFaceDetectImageOnePerson.png",
-            "src/test/resources/detectResults/TestFaceDetectResultOnePerson_2.png"
-            );
-
 
         } catch (DetectException e) {
             e.printStackTrace();
--- src/test/java/FaceDetectingAndCuttingTest.java
+++ src/test/java/FaceDetectingAndCuttingTest.java
@@ -1,23 +1,19 @@
 import eleron.jackal.DetectException;
 import eleron.jackal.Detector;
 import eleron.jackal.IDetectable;
-import eleron.jackal.JackalTypes;
 
 public class FaceDetectingAndCuttingTest {
     public static void main(String[] args) {
         try {
-            IDetectable detector = new Detector();
+            Detector detector = new Detector();
             detector.faceDetectAndCut(
                 "src/test/resources/main/TestFaceDetectImage.png",
-                "src/test/resources/cuttingResults/TestFaceDetectImageCuttingResult.png",
-                    JackalTypes.FRONTAL_FACE
+                "src/test/resources/cuttingResults/TestFaceDetectImageCuttingResult.png"
             );
 
-            IDetectable detector2 = new Detector();
             detector.faceDetectAndCut(
-                    "src/test/resources/main/TestFaceDetectImageOnePerson.png",
-                    "src/test/resources/cuttingResults/TestFaceDetectImageCuttingResult2.png",
-                    JackalTypes.FRONTAL_FACE_2
+                "src/test/resources/main/TestFaceDetectImageOnePerson.png",
+                "src/test/resources/cuttingResults/TestFaceDetectImageCuttingResult2.png"
             );
 
         } catch (DetectException e) {
