8487131a03e8c64e9d44f059525dc2ab0f7ca2f1
HvarZ <hvarzahar@gmail.com>
2022-07-20 14:36:02 +0300

    add new constants

--- src/main/java/eleron/jackal/JackalTypes.java
+++ src/main/java/eleron/jackal/JackalTypes.java
@@ -1,7 +1,22 @@
 package eleron.jackal;
 
+import org.opencv.core.Scalar;
+
 public final class JackalTypes {
-    public static final int DEFAULT = 0;
-    public static final int FRONTAL_FACE = 1;
-    public static final int FRONTAL_FACE_2 = 2;
+    public final static int IMAGE_GREY_LEVEL = 0;
+
+    public final static int HIERARCHY_NOT_FOUND = -1;
+    public final static int HIERARCHY_NEXT = 0;
+    public final static int HIERARCHY_PREV = 1;
+    public final static int HIERARCHY_PARENT = 2;
+    public final static int HIERARCHY_CHILD = 3;
+
+    public final static Scalar WHITE = new Scalar(255, 255, 255);
+    public final static Scalar RED = new Scalar(0, 0, 255);
+    public final static Scalar GREEN = new Scalar(0, 255, 0);
+    public final static Scalar BLUE = new Scalar(255, 0, 0);
+
+    public final static int MAX_BRIGHTNESS = 255;
+
+    public final static int KERNEL_SIZE = 3;
 }
