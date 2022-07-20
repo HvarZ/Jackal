1edad921fd46d11f46de9cef39484ba86f637c62
HvarZ <hvarzahar@gmail.com>
2022-07-20 14:35:24 +0300

    remove cascade variant for detecting

--- src/main/java/eleron/jackal/IDetectable.java
+++ src/main/java/eleron/jackal/IDetectable.java
@@ -1,7 +1,7 @@
 package eleron.jackal;
 
 public interface IDetectable {
-    void faceDetectAndCut(String pathRead, String pathWrite, int cascadeVariant) throws DetectException;
+    void faceDetectAndCut(String pathRead, String pathWrite) throws DetectException;
 
     void magicWand(String pathRead, String pathWrite, int[] kernelSettings, int[] preparingSettings) throws DetectException;
 
