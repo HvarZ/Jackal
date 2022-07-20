f3f6a1bbcda11a850f8681e24e3e4962e1c26adf
HvarZ <hvarzahar@gmail.com>
2022-07-20 11:35:07 +0300

    rename method findAndBlur -> backgroundBlur

--- src/main/java/eleron/jackal/IDetectable.java
+++ src/main/java/eleron/jackal/IDetectable.java
@@ -1,11 +1,9 @@
 package eleron.jackal;
 
 public interface IDetectable {
-    void faceDetect(String pathRead, String pathWrite, int cascadeVariant) throws DetectException;
-
     void faceDetectAndCut(String pathRead, String pathWrite, int cascadeVariant) throws DetectException;
 
     void magicWand(String pathRead, String pathWrite, int[] kernelSettings, int[] preparingSettings) throws DetectException;
 
-    void findAndBlur(String pathRead, String pathWrite) throws DetectException;
+    void backgroundBlur(String pathRead, String pathWrite) throws DetectException;
 }
