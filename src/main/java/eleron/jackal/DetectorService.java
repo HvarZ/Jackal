852e8e74c713781d858518bafd257b59d0da8fc0
hvarz <hvarzahar@gmail.com>
2022-07-21 10:34:36 +0300

    add some functions for converters

--- src/main/java/eleron/jackal/DetectorService.java
+++ src/main/java/eleron/jackal/DetectorService.java
@@ -4,7 +4,7 @@ import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -127,5 +127,117 @@ final class DetectorService {
         imageRead.release();
 
         return greyImage;
+    }
+
+    public static byte[] saveImageBinary(Mat image) throws DetectException {
+        if (image == null || image.empty()) {
+            throw new DetectException("saveImageBinary: image is empty");
+        }
+
+        if (image.depth() == CvType.CV_8U) {
+        } else if (image.depth() == CvType.CV_16U) {
+            Mat m_16 = new Mat();
+            image.convertTo(m_16, CvType.CV_8U, 255.0 / 65535);
+            image = m_16;
+        } else if (image.depth() == CvType.CV_32F) {
+            Mat m_32 = new Mat();
+            image.convertTo(m_32, CvType.CV_8U, 255);
+            image = m_32;
+        } else {
+            throw new DetectException("saveImageBinary: unknown image type");
+        }
+
+        if (image.channels() == 2 || image.channels() > 4) {
+            throw new DetectException("saveImageBinary: invalid numbers of image channels");
+        }
+
+        byte[] imageParameters = getBytesImageParameters(image.rows(), image.cols(), image.channels());
+        byte[] buffer = new byte[image.channels() * image.cols() * image.rows()];
+
+        image.get(0, 0, buffer);
+
+        return mergeBytes(imageParameters, buffer);
+    }
+
+    public static Mat loadMatBinary(byte[] imageBinary) throws DetectException {
+        if (imageBinary.length == 0) {
+            throw new DetectException("loadMatBinary: imageBinary is empty");
+        }
+
+        int rows = getParameterFromBytes(imageBinary, 0);
+        if (rows < 1) {
+            throw new DetectException("loadMatBinary: invalid rows number");
+        }
+
+        int cols = getParameterFromBytes(imageBinary, 2);
+        if (cols < 1) {
+            throw new DetectException("loadMatBinary: invalid cols number");
+        }
+
+        int channels = getParameterFromBytes(imageBinary, 4);
+        int type;
+        if (channels == 1) {
+            type = CvType.CV_8UC1;
+        } else if (channels == 3) {
+            type = CvType.CV_8UC1;
+        } else if (channels == 4) {
+            type = CvType.CV_8UC4;
+        } else {
+            return new Mat();
+        }
+        Mat image = new Mat(rows, cols, type);
+
+        removeParameters(imageBinary);
+        image.put(0, 0, imageBinary);
+
+        return image;
+    }
+
+    public static byte[] convertIntToByte(int number) throws DetectException {
+        if (number > 32_767) {
+            throw new DetectException("convertIntToByte: Number is too large");
+        } else if (number < 0) {
+            throw new DetectException("convertIntToByte: Number is negative");
+        }
+        byte[] result = new byte[JackalTypes.INT_IMP_BYTES];
+        result[0] = (byte) (number & 0xFF);
+        result[1] = (byte) ((number >> 8) & 0xFF);
+
+        return result;
+    }
+
+    public static byte[] getBytesImageParameters(int rows, int cols, int channels) throws DetectException {
+        byte[] rowsBytes = convertIntToByte(rows);
+        byte[] colsBytes = convertIntToByte(cols);
+        byte[] channelsBytes = convertIntToByte(channels);
+        byte[] result = mergeBytes(rowsBytes, colsBytes);
+
+        return mergeBytes(result, channelsBytes);
+    }
+
+    public static int getParameterFromBytes(byte[] imageBytes, int indexStart) {
+        byte[] numberByte = new byte[2];
+        numberByte[0] = imageBytes[indexStart];
+        numberByte[1] = imageBytes[indexStart + 1];
+
+        return convertByteToInt(numberByte);
+    }
+
+    public static int convertByteToInt(byte[] number) {
+        int high = number[1] >= 0 ? number[1] : 256 + number[1];
+        int low = number[0] >= 0 ? number[0] : 256 + number[0];
+
+        return low | (high << 8);
+    }
+
+    public static byte[] mergeBytes(byte[] first, byte[] second) {
+        byte[] result = new byte[first.length + second.length];
+        System.arraycopy(first, 0, result, 0, first.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+
+    public static void removeParameters(byte[] imageBytes) {
+        System.arraycopy(imageBytes, 6, imageBytes, 0, imageBytes.length - 6);
     }
 }
