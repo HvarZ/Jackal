fcdc08b7e28a0a1d2eb616c7c3dead7136783f35
hvarz <hvarzahar@gmail.com>
2022-07-21 11:33:47 +0300

    add convert test

---
+++ src/test/java/ConvertTest.java
@@ -0,0 +1,21 @@
+import eleron.jackal.DetectException;
+import eleron.jackal.Detector;
+import eleron.jackal.IDetectable;
+
+public class ConvertTest {
+    public static void main(String[] args) {
+        try {
+            IDetectable detector = new Detector();
+            byte[] imageBytes = detector.backgroundBlur(
+                "src/test/resources/main/TestFaceDetectImageOnePerson.png"
+            );
+
+            Detector.convertBytesToImage(
+                    imageBytes,
+                    "src/test/resources/convertingResults/converting.png"
+            );
+        } catch (DetectException e) {
+            e.printStackTrace();
+        }
+    }
+}
