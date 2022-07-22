a40fe4ec9d4d2b4b9213f9e9a224157e204f953e
hvarz <hvarzahar@gmail.com>
2022-07-22 09:48:52 +0300

    delete unused constants

--- src/main/java/eleron/jackal/JackalTypes.java
+++ src/main/java/eleron/jackal/JackalTypes.java
@@ -1,23 +1,13 @@
 package eleron.jackal;
 
-import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 
 public final class JackalTypes {
-    public final static int IMAGE_GREY_LEVEL = 0;
-
-    public final static int HIERARCHY_NOT_FOUND = -1;
-    public final static int HIERARCHY_NEXT = 0;
-    public final static int HIERARCHY_PREV = 1;
-    public final static int HIERARCHY_PARENT = 2;
-    public final static int HIERARCHY_CHILD = 3;
-
-    public final static Scalar WHITE = new Scalar(255, 255, 255);
-    public final static Scalar RED = new Scalar(0, 0, 255);
-    public final static Scalar GREEN = new Scalar(0, 255, 0);
-    public final static Scalar BLUE = new Scalar(255, 0, 0);
-
-    public final static int MAX_BRIGHTNESS = 255;
-
-    public final static int KERNEL_SIZE = 3;
     public final static byte INT_IMP_BYTES = 2;
+
+    public final static double X_SCALE_COEFFICIENT = 1.5;
+    public final static double Y_SCALE_COEFFICIENT = 2.25;
+
+    public static Size BLUR_SQUARE = new Size(41, 41);
+    public static int BLUR_SIGMA = 140;
 }
