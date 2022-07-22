80f7790cb2e399078900fa687d3cfcd565697c0b
hvarz <hvarzahar@gmail.com>
2022-07-22 13:03:44 +0300

    use nio converters

--- src/main/java/eleron/jackal/Converter.java
+++ src/main/java/eleron/jackal/Converter.java
@@ -1,82 +1,31 @@
 package eleron.jackal;
 
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Converter {
-    public static void convertBytesToImage(final byte[] imagesBytes, String pathWrite) throws DetectException {
-        Mat image = loadMatBinary(imagesBytes);
-        Imgcodecs.imwrite(pathWrite, image);
+    public static void convertBytesToImage(final byte[] imageBytes, File output) throws DetectException {
+        Mat image = Imgcodecs.imdecode(new MatOfByte(imageBytes), Imgcodecs.IMREAD_UNCHANGED);
+        Imgcodecs.imwrite(output.getPath(), image);
     }
 
-    public static byte[] convertImageToBytes(String pathRead) throws DetectException {
-        Mat image = Imgcodecs.imread(pathRead);
-        return saveImageBinary(image);
+    public static byte[] convertImageToBytes(File file) throws IOException {
+        return Files.readAllBytes(file.toPath());
     }
+
 
     protected static byte[] saveImageBinary(Mat image) throws DetectException {
         if (image == null || image.empty()) {
             throw new DetectException("saveImageBinary: image is empty");
         }
+        MatOfByte imageArray = new MatOfByte();
+        Imgcodecs.imencode(JackalTypes.SAVE_TYPE, image, imageArray);
 
-        if (image.depth() == CvType.CV_8U) {
-        } else if (image.depth() == CvType.CV_16U) {
-            Mat m_16 = new Mat();
-            image.convertTo(m_16, CvType.CV_8U, 255.0 / 65535);
-            image = m_16;
-        } else if (image.depth() == CvType.CV_32F) {
-            Mat m_32 = new Mat();
-            image.convertTo(m_32, CvType.CV_8U, 255);
-            image = m_32;
-        } else {
-            throw new DetectException("saveImageBinary: unknown image type");
-        }
-
-        if (image.channels() == 2 || image.channels() > 4) {
-            throw new DetectException("saveImageBinary: invalid numbers of image channels");
-        }
-
-        byte[] imageParameters = DetectorService.getBytesImageParameters(image.rows(), image.cols(), image.channels());
-        byte[] buffer = new byte[image.channels() * image.cols() * image.rows()];
-
-        image.get(0, 0, buffer);
-
-        return DetectorService.mergeBytes(imageParameters, buffer);
-    }
-
-    protected static Mat loadMatBinary(final byte[] imageBinary) throws DetectException {
-        if (imageBinary.length == 0) {
-            throw new DetectException("loadMatBinary: imageBinary is empty");
-        }
-
-        int rows = DetectorService.getParameterFromBytes(imageBinary, 0);
-        if (rows < 1) {
-            throw new DetectException("loadMatBinary: invalid rows number");
-        }
-
-        int cols = DetectorService.getParameterFromBytes(imageBinary, 2);
-        if (cols < 1) {
-            throw new DetectException("loadMatBinary: invalid cols number");
-        }
-
-        int channels = DetectorService.getParameterFromBytes(imageBinary, 4);
-        int type;
-        if (channels == 1) {
-            type = CvType.CV_8UC1;
-        } else if (channels == 3) {
-            type = CvType.CV_8UC3;
-        } else if (channels == 4) {
-            type = CvType.CV_8UC4;
-        } else {
-            return new Mat();
-        }
-        Mat image = new Mat(rows, cols, type);
-
-        byte[] imageWithoutParameters = imageBinary.clone();
-        DetectorService.removeParameters(imageWithoutParameters);
-        image.put(0, 0, imageWithoutParameters);
-
-        return image;
+        return imageArray.toArray();
     }
 }
