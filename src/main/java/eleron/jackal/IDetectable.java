5680b2ac3814d8dd5c557965b3aed37190d12cba
hvarz <hvarzahar@gmail.com>
2022-07-22 10:03:03 +0300

    add new interface

--- src/main/java/eleron/jackal/IDetectable.java
+++ src/main/java/eleron/jackal/IDetectable.java
@@ -1,15 +1,9 @@
 package eleron.jackal;
 
 public interface IDetectable {
-    void faceDetectAndCut(String pathRead, String pathWrite) throws DetectException;
+    void detect(String pathRead, String pathWrite, Modes mode) throws DetectException;
 
-    void magicWand(String pathRead, String pathWrite, int[] kernelSettings, int[] preparingSettings) throws DetectException;
+    byte[] detect(String pathRead, Modes mode) throws DetectException;
 
-    void backgroundBlur(String pathRead, String pathWrite) throws DetectException;
-
-    byte[] backgroundBlur(String pathRead) throws DetectException;
-
-    byte[] backgroundBlur(byte[] imageBytes) throws DetectException;
-
-
+    byte[] detect(byte[] imageBytes, Modes mode) throws DetectException;
 }
