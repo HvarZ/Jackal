af962dc42ddbed5fc893d8f813e3cb0f2be50652
hvarz <hvarzahar@gmail.com>
2022-07-22 09:48:23 +0300

    fill convertTest

--- src/test/java/ConvertTest.java
+++ src/test/java/ConvertTest.java
@@ -1,14 +1,12 @@
-import eleron.jackal.Converter;
-import eleron.jackal.DetectException;
-import eleron.jackal.Detector;
-import eleron.jackal.IDetectable;
+import eleron.jackal.*;
 
 public class ConvertTest {
     public static void main(String[] args) {
         try {
             IDetectable detector = new Detector();
-            byte[] imageBytes = detector.backgroundBlur(
-                "src/test/resources/main/TestFaceDetectImageOnePerson.png"
+            byte[] imageBytes = detector.detect(
+                "src/test/resources/main/1.jpg",
+                    Modes.Blurring
             );
 
             Converter.convertBytesToImage(
@@ -25,10 +23,35 @@ public class ConvertTest {
                     "src/test/resources/convertingResults/convertingNormal.png"
             );
 
-            byte[] imageBlurBytes = detector.backgroundBlur(imageBytes2);
+            byte[] imageBlurBytes = detector.detect(imageBytes2, Modes.Blurring);
             Converter.convertBytesToImage(
                     imageBlurBytes,
                     "src/test/resources/convertingResults/convertingBlur2.png"
+            );
+
+            byte[] imageBytes3 = detector.detect(
+                    "src/test/resources/main/1.jpg",
+                    Modes.Cutting
+            );
+
+            Converter.convertBytesToImage(
+                    imageBytes3,
+                    "src/test/resources/convertingResults/convertingBlur3.png"
+            );
+
+            byte[] imageBytes4 =  Converter.convertImageToBytes(
+                    "src/test/resources/main/TestFaceDetectImageOnePerson.png"
+            );
+
+            Converter.convertBytesToImage(
+                    imageBytes4,
+                    "src/test/resources/convertingResults/convertingNormal4.png"
+            );
+
+            byte[] imageBlurBytes5 = detector.detect(imageBytes2, Modes.Cutting);
+            Converter.convertBytesToImage(
+                    imageBlurBytes5,
+                    "src/test/resources/convertingResults/convertingBlur5.png"
             );
         } catch (DetectException e) {
             e.printStackTrace();
