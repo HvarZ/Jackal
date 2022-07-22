4d25d5b3f21081d4e9e904fda42b8263e7963a6a
hvarz <hvarzahar@gmail.com>
2022-07-22 14:42:09 +0300

    add default constructor

--- src/main/java/eleron/jackal/DetectException.java
+++ src/main/java/eleron/jackal/DetectException.java
@@ -1,6 +1,9 @@
 package eleron.jackal;
 
 public class DetectException extends Exception {
+   public DetectException() {
+      super();
+   }
    public DetectException(String message) {
       super(message);
    }
