6c984bf8366001f9e2fc14371764d2dc551bcffb
hvarz <hvarzahar@gmail.com>
2022-07-22 09:47:11 +0300

    delete unused function

--- src/main/java/eleron/jackal/DetectorService.java
+++ src/main/java/eleron/jackal/DetectorService.java
@@ -1,75 +1,9 @@
 package eleron.jackal;
 
 import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 final class DetectorService {
-    public static void setBlackBackground(Mat image, Mat mask) throws DetectException {
-        if (image.height() != mask.height() || image.width() != mask.width()) {
-            throw new DetectException("SetBlackBackground: Size of image is not equal to size of mask");
-        }
-        for (int i = 0; i < mask.rows(); ++i) {
-            for (int j = 0; j < mask.cols(); ++j) {
-                if (mask.get(i, j)[JackalTypes.IMAGE_GREY_LEVEL] == 0) {
-                    image.put(i, j, new byte[image.channels()]);
-                }
-            }
-        }
-    }
-
-    public static void fillNestedContour(Mat img, List<MatOfPoint> contours, Mat hierarchy) throws DetectException {
-        if (contours.size() != hierarchy.cols()) {
-            throw new DetectException("fillNestedContour: Length of contours is not equal to hierarchy size");
-        }
-
-        List<MatOfPoint> fillingContours = new ArrayList<>();
-        for (int i = 0; i < hierarchy.rows(); ++i) {
-            for (int j = 0; j < hierarchy.cols(); ++j) {
-                if (hierarchy.get(i, j)[JackalTypes.HIERARCHY_PARENT] != JackalTypes.HIERARCHY_NOT_FOUND) {
-                    fillingContours.add(contours.get(j));
-                }
-            }
-        }
-
-        Imgproc.fillPoly(img, fillingContours, JackalTypes.WHITE);
-    }
-
-
-    public static void prepareImage(Mat srcImage, Mat resultImage, Size kernelSize, int thresholdBinary) {
-        Mat blurImage = new Mat();
-        Mat binaryImage = new Mat();
-
-        Imgproc.blur(srcImage,
-                blurImage,
-                kernelSize);
-
-        Imgproc.threshold(blurImage,
-                binaryImage,
-                thresholdBinary,
-                JackalTypes.MAX_BRIGHTNESS,
-                Imgproc.THRESH_BINARY_INV);
-
-        Imgproc.morphologyEx(binaryImage,
-                resultImage,
-                Imgproc.MORPH_CLOSE,
-                Imgproc.getStructuringElement(Imgproc.MORPH_RECT, kernelSize));
-    }
-
-    public static void checkValidity(String path, int[] kernel, boolean isNeededKernel) throws DetectException {
-        if (!(new File(path).exists())) {
-            throw new DetectException("File (" + path + ") not found");
-        }
-
-        if (isNeededKernel && kernel != null && kernel.length != JackalTypes.KERNEL_SIZE) {
-            throw new DetectException("Invalid size of kernelSettings: != " + JackalTypes.KERNEL_SIZE);
-        }
-    }
-
     public static void scaleRect(Rect rect, double xCoefficient, double yCoefficient) throws DetectException {
         if (xCoefficient <= 0) {
             throw new DetectException("scaleRect: invalid argument (xCoefficient must be > 0)");
@@ -112,22 +46,6 @@ final class DetectorService {
             throw new DetectException("getCentre: rect is empty");
         }
         return new Point(rect.x + (double) rect.width / 2, rect.y + (double) rect.height / 2);
-    }
-
-
-    public static Mat getGrayMat(String pathRead, String pathWrite) throws DetectException {
-        checkValidity(pathRead, null, false);
-
-        Mat imageRead = Imgcodecs.imread(pathRead);
-        Mat greyImage = new Mat(imageRead.size(), imageRead.type());
-
-        Imgproc.cvtColor(imageRead, greyImage, Imgproc.COLOR_BGR2GRAY);
-
-        Imgcodecs.imwrite(pathWrite, greyImage);
-
-        imageRead.release();
-
-        return greyImage;
     }
 
     public static byte[] convertIntToByte(int number) throws DetectException {
