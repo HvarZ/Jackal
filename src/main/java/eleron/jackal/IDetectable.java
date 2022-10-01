ebe364a634cef8e935c2a4bbd7258f764a4ef74b
hvarz <hvarzahar@gmail.com>
2022-10-01 03:39:00 +0300

    add javadoc comments

--- src/main/java/eleron/jackal/IDetectable.java
+++ src/main/java/eleron/jackal/IDetectable.java
@@ -7,9 +7,9 @@ public interface IDetectable {
         Метод, реализующий поиск лиц/лица на фотографии в зависимости от режима работы делает следующий функционал:
             1. Modes.Blurring - блюр фона за прямоугольника лица
             2. Modes.Cutting - вырезка прямоугольника лица
-        input - File объект, искомое изображение
-        output - File объект, результирующее изображение
-        Исключение выбрасывается при отсутсвии input-файла
+        @param input - File объект, искомое изображение
+        @param output - File объект, результирующее изображение
+        @throws DetectException Исключение выбрасывается при отсутсвии input-файла
      */
     void detect(File input, File output, Modes mode) throws DetectException;
 
@@ -17,9 +17,9 @@ public interface IDetectable {
         Метод, реализующий поиск лиц/лица на фотографии в зависимости от режима работы делает следующий функционал:
             1. Modes.Blurring - блюр фона за прямоугольника лица
             2. Modes.Cutting - вырезка прямоугольника лица
-        input - File объект, искомое изображение
-        return - byte[] объект, результирующее изображение
-        Исключение выбрасывается при отсутсвии input-файла
+        @param input - File объект, искомое изображение
+        @return byte[] объект, результирующее изображение
+        @throws DetectException Исключение выбрасывается при отсутсвии input-файла
      */
     byte[] detect(File input, Modes mode) throws DetectException;
 
@@ -27,9 +27,9 @@ public interface IDetectable {
         Метод, реализующий поиск лиц/лица на фотографии в зависимости от режима работы делает следующий функционал:
             1. Modes.Blurring - блюр фона за прямоугольника лица
             2. Modes.Cutting - вырезка прямоугольника лица
-        imageBytes - byte[] объект, искомое изображение
-        return - byte[] объект, результирующее изображение
-        Исключение выбрасывается при отсутсвии input-файла
+        @param imageBytes - byte[] объект, искомое изображение
+        @return - byte[] объект, результирующее изображение
+        @throws DetectException Исключение выбрасывается при отсутсвии input-файла
      */
     byte[] detect(byte[] imageBytes, Modes mode) throws DetectException;
 }
